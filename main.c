46a237ebb9626d5dcfa0a19063cb19e003a55679
Conner Wolfe <00007186@jasperengines.com>
2020-02-27 17:16:21 -0500

    Got compiler figured out, added a little bit of code

--- main.c
+++ main.c
@@ -1,2 +1,10 @@
 #include <stdio.h>
+#include "validator.h"
 
+int main(){
+    int creditCardNumber;
+    printf("Please enter a credit card number: ");
+    scanf("%i", creditCardNumber);
+    
+    return 0;
+}
